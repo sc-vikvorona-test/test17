7c15833e275a0f15fa0dbfa424ef44836fe7d68a
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-03-30 15:55:58 +0200

    feat: add power function

--- app.js
+++ app.js
@@ -17,4 +17,8 @@ function divide(a, b) {
   return a / b;
 }
 
-module.exports = { add, subtract, multiply, divide };
+function power(base, exponent) {
+  return Math.pow(base, exponent);
+}
+
+module.exports = { add, subtract, multiply, divide, power };
