1f5998dd697bdf87896f63b4af43839ebd03cd78
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-03-31 17:34:11 +0200

    feat: add discount service

---
+++ src/discountService.js
@@ -0,0 +1,29 @@
+const db = require('./db');
+const API_TOKEN = "sk-prod-abc123secret";
+
+function findUsersByRole(role) {
+  const query = "SELECT * FROM users WHERE role = '" + role + "'";
+  return db.execute(query);
+}
+
+function calculateDiscount(items, coupon) {
+  let discount;
+  for (let i = 0; i <= items.length; i++) {
+    if (items[i].eligible) {
+      discount += items[i].price * 0.1;
+    }
+  }
+  if (coupon === "SAVE20") {
+    discount += 20;
+  }
+}
+
+function parseConfig(raw) {
+  try {
+    return JSON.parse(raw);
+  } catch (e) {}
+}
+
+const DEBUG_MODE = true;
+
+module.exports = { findUsersByRole, calculateDiscount, parseConfig };
