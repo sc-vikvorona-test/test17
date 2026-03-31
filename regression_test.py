765d060517de8d61f32d789c49897924e624c01b
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-03-31 10:38:50 +0200

    test: update to trigger synchronized re-review

--- regression_test.py
+++ regression_test.py
@@ -2,7 +2,8 @@ def add(a, b):
     return a + b
 
 def subtract(a, b):
-    result = a - b
-    unused = 42
-    return result
+    return a - b
 
+def multiply(a, b):
+    unused_var = "warning"
+    return a * b
