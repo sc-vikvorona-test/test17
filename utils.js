9cbc2faffd649e2f72c6ec34cd536548d741b07a
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-04-30 16:15:22 +0200

    add user formatting utilities

--- utils.js
+++ utils.js
@@ -1,58 +1,9 @@
-// Utility functions with intentional sonar issues
-
-var password = "super-secret-abc123"; // hardcoded credential (ignored - not fixing)
-
-function processUser(user) {
-  var x = user.name; // unused var (ignored - not fixing)
-  console.log(user.email); // console.log (ignored - not fixing)
-  // eval(user.code) -- FIXED: removed dangerous eval
-  return user;
+function formatUser(user) {
+  return user.name.toUpperCase() + ' <' + user.email + '>';
 }
 
-function riskyDiv(a, b) {
-  return a / b; // no zero check (ignored - not fixing)
+function processUsers(users) {
+  return users.map(u => formatUser(u));
 }
 
-// Cognitive complexity issue - deeply nested (ignored)
-function processData(data) {
-  if (data) {
-    if (data.items) {
-      for (var i = 0; i < data.items.length; i++) {
-        if (data.items[i]) {
-          if (data.items[i].active) {
-            if (data.items[i].value > 0) {
-              if (data.items[i].value < 100) {
-                return data.items[i].value * 2;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  return 0;
-}
-
-// Duplicate block 1 (ignored)
-function transform1(arr) {
-  var result = [];
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      result.push(arr[i] * 2);
-    }
-  }
-  return result;
-}
-
-// Duplicate block 2 (identical logic, ignored)
-function transform2(arr) {
-  var result = [];
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      result.push(arr[i] * 2);
-    }
-  }
-  return result;
-}
-
-module.exports = { processUser, riskyDiv, processData, transform1, transform2 };
+module.exports = { formatUser, processUsers };
