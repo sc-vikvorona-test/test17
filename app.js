7cfb8a1a5d87c3945b3240d0cfda5d9c21eb4921
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-05-04 15:46:46 +0200

    feat: integrate auth module into app

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { hashPassword, validateUser, generateToken } = require('./auth');
+
 function add(a, b) {
   return a + b;
 }
@@ -12,9 +14,16 @@ function multiply(a, b) {
 
 function divide(a, b) {
   if (b === 0) {
-    throw new Error('Division by zero');
+    throw new Error("Division by zero");
   }
   return a / b;
 }
 
-module.exports = { add, subtract, multiply, divide };
+function createSession(username, password) {
+  if (!validateUser(username, password)) {
+    return null;
+  }
+  return generateToken(username, "user", Date.now() + 3600000);
+}
+
+module.exports = { add, subtract, multiply, divide, createSession };
